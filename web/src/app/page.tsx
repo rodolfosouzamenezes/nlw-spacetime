202af6b9ad7e22a511beba5d7b050eeae39b2044
Rodolfo Menezes <rodolfosouzaofc@gmail.com>
2023-11-28 10:39:41 -0300

    feat(web): setup

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">
+        DIVISORIA
+        </h1>
+    </div>
+  )
+}
