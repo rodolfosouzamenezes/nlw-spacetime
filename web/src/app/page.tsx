d9028f29a38a62170d341b46bca9a69fb8fad196
Rodolfo Menezes <rodolfosouzaofc@gmail.com>
2023-11-29 10:18:56 -0300

    feat(web): fonts added

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,7 +3,10 @@ export default function Home() {
     <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
       <h1 className="text-4xl font-bold">
         DIVISORIA
-        </h1>
+      </h1>
+      <h1 className="text-4xl font-bold font-alt">
+        DIVISORIA
+      </h1>
     </div>
   )
 }
