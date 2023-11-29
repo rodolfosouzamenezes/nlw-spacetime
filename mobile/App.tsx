d034e9a33f9b4618574980182de68d616eaba534
Rodolfo Menezes <rodolfosouzaofc@gmail.com>
2023-11-29 09:27:43 -0300

    feat(mobile): setup

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center bg-gray-950'>
+      <Text className='text-zinc-50 font-bold text-5xl'>HAHAHAHAHHAHAHAHHAHAHAHHAHAHAAHHA</Text>
+      <StatusBar style="light" translucent />
+    </View>
+  );
+}
