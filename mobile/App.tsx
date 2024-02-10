4c0c1020173559ac078193f6757575187f73f1db
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-02-10 13:52:00 -0300

    feat(mobile): get github token

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from '@expo-google-fonts/roboto';
+import * as SecureStore from 'expo-secure-store';
 
 import blurBg from './src/assets/bg-blur.png';
 import NlwLogo from './src/assets/nlw-spacetime-logo.svg';
@@ -9,9 +10,52 @@ const StyledStripes = styled(Stripes)
 
 import { fonts } from './src/fonts';
 import { styled } from 'nativewind';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
+import { api } from './src/lib/axios';
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/8e4a456a09fdcd1fcb64',
+};
 
 export default function App() {
   const [hasLoadedFonts] = useFonts(fonts)
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '8e4a456a09fdcd1fcb64',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    // console.log(makeRedirectUri({
+    //   scheme: 'nlwspacetime'
+    // }),);
+
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+      api.post('/auth', {
+        code
+      }).then((res) => {
+        const {token} = res.data
+        
+        SecureStore.setItemAsync('token', token)
+        console.log(token);
+        
+      }).catch((err) => {
+        console.log(err)
+      })
+      
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null
@@ -37,8 +81,9 @@ export default function App() {
         </View>
 
         <TouchableOpacity
-          className='rounded-full bg-green-500 px-5 py-2'
+          onPress={() => signInWithGithub()}
           activeOpacity={0.7}
+          className='rounded-full bg-green-500 px-5 py-2'
         >
           <Text className='font-alt text-sm uppercase text-black'>
             Cadastrar lembrança
