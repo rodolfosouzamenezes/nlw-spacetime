fa62a8c22abb56c1ae9df8b0c66669564d2c3290
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-02-13 06:58:20 -0300

    feat(mobile): fetch memories list

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
             <Copyrigth />
           </div>
 
-          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+          <div className="flex max-h-screen overflow-y-scroll flex-col bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,63 @@
-import { EmptyMemories } from '@/components/EmptyMemories'
+import { cookies } from "next/headers";
+import ptBr from "dayjs/locale/pt-br";
+import dayjs from "dayjs";
 
-export default function Home() {
-  return <EmptyMemories />
+dayjs.locale(ptBr);
+
+import { EmptyMemories } from "@/components/EmptyMemories";
+import { api } from "@/lib/api";
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+
+interface IMemory {
+  id: string;
+  coverUrl: string;
+  expert: string;
+  createdAt: string;
+}
+
+export default async function Home() {
+  const token = cookies().get("token")?.value;
+
+  if (!token) return <EmptyMemories />;
+
+  const response = await api.get<IMemory[]>("/memories", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const memories = response.data;
+
+  if (memories.length === 0) return <EmptyMemories />;
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((m) => {
+        return (
+          <div key={m.id} className="space-y-4">
+            <time className="flex items-center gap-2 text-sm text-gray-100 -ml-8 before:h-px before:w-5 before:bg-gray-50">
+              {dayjs(m.createdAt).format("D[ de ]MMMM[, ]YYYY")}
+            </time>
+            <Image 
+              src={m.coverUrl}
+              alt=""
+              width={592}
+              height={280}
+              quality={100}
+              className="w-full aspect-video object-cover rounded-lg"
+            />
+            <p className="text-lg leading-relaxed text-gray-100 line-clamp-5">
+              {m.expert}
+            </p>
+            <Link href={`/memories/${m.id}`} className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100">
+              Ler mais
+              <ArrowRight className="w-4 h-4" />
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
